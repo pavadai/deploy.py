6f74c9e8d1a6225c82eb97a5cd54dc434224f431
pavadai <manishapavadai13@gmail.com>
2025-10-15 16:01:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"]
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='https://sl.bing.net/hC1OaODIfzE', use_column_width=True)
     
     st.write("Click on the image to detect color.")
     
